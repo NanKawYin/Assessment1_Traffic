30311eff6594cd3556254d2195be0fab98fd9da7
NanKawYin <kawyin.nan@my.jcu.edu.au>
2020-01-24 21:06:09 +0800

    "Commit"

--- src/Objects/Road_Block.java
+++ src/Objects/Road_Block.java
@@ -2,8 +2,8 @@ package Objects;
 
 public class Road_Block extends Simulator_Obj{
 
-    public Road_Block(){
-        super("Road Block", "/Users/user/Desktop/Assessment_NanKaw/src/Photo/Road.jpg",
-                true, null, 'N', false, null);
+    public Road_Block(char[] direction, String pic_location){
+        super("Road Block", pic_location,
+                true, null, direction, false, null);
     }
 }
